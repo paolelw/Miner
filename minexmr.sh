26241453e5d8cc895f761256244d4c4385bded27
Kevin Liu <gscsnm@users.noreply.github.com>
2020-06-02 18:22:10 +0800

    Update minexmr.sh

--- minexmr.sh
+++ minexmr.sh
@@ -13,7 +13,8 @@ make -j$(nproc)
 # go to the build directory and run git pull.
 
 #运行
-./xmrig --donate-level 1 -o pool.minexmr.com:443 -u 89xFXepSy2iBW1cMgaQwZwedkgDvAJcJk7UMyHYy1TsZA9aw9YhYWeobXn5mXQgx9eMduAdFrfXEGJaKEZ43eZWm7xeMCyi -k --tls --rig-id ly180_74
+
+echo "./xmrig --donate-level 1 -o pool.minexmr.com:443 -u 89xFXepSy2iBW1cMgaQwZwedkgDvAJcJk7UMyHYy1TsZA9aw9YhYWeobXn5mXQgx9eMduAdFrfXEGJaKEZ43eZWm7xeMCyi -k --tls --rig-id xxx"
 
 
 
