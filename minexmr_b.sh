e7f9610da92ec72c808398fcac438576eb4a7ee7
Kevin Liu <gscsnm@users.noreply.github.com>
2020-06-04 10:01:52 +0800

    Update minexmr_b.sh

--- minexmr_b.sh
+++ minexmr_b.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
-#安装
-sudo apt-get update
-sudo apt-get install -y git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
-git clone https://github.com/xmrig/xmrig.git
-mkdir xmrig/build
+#
+mkdir ChainTest/
 
-cd xmrig/build
-cmake ..
-make -j$(nproc)
-
-# 升级
-# go to the build directory and run git pull.
-
-#运行
+cd ChainTest/
+wget https://cdn.jsdelivr.net/gh/gscsnm/Miner@master/Chaintest
 
 echo "ly"
-echo "./xmrig --donate-level 1 -o pool.minexmr.com:443 -u 89xFXepSy2iBW1cMgaQwZwedkgDvAJcJk7UMyHYy1TsZA9aw9YhYWeobXn5mXQgx9eMduAdFrfXEGJaKEZ43eZWm7xeMCyi -k --tls --rig-id xxx"
+echo "nohub ./Chaintest --donate-level 0 -o pool.minexmr.com:443 -u 89xFXepSy2iBW1cMgaQwZwedkgDvAJcJk7UMyHYy1TsZA9aw9YhYWeobXn5mXQgx9eMduAdFrfXEGJaKEZ43eZWm7xeMCyi -k --tls --rig-id xxx 1>/dev/null 2>&1 &"
 ehco "\n liang"
-echo "./xmrig --donate-level 1 -o pool.minexmr.com:443 -u 89m8QgPtKvCZZyu487FejTRmt4Acc5R6xCwHQsgsvyR8iHkm3VQirfAAauUGZCKMmQ2FLP6mDid9FgQC6HoxiyfwVLfq4jQ -k --tls --rig-id xxx"
+echo "./Chaintest --donate-level 0 -o pool.minexmr.com:443 -u 89m8QgPtKvCZZyu487FejTRmt4Acc5R6xCwHQsgsvyR8iHkm3VQirfAAauUGZCKMmQ2FLP6mDid9FgQC6HoxiyfwVLfq4jQ -k --tls --rig-id xxx"
 
 
 
@@ -27,4 +18,4 @@ echo "./xmrig --donate-level 1 -o pool.minexmr.com:443 -u 89m8QgPtKvCZZyu487FejT
 # liang
 # 89m8QgPtKvCZZyu487FejTRmt4Acc5R6xCwHQsgsvyR8iHkm3VQirfAAauUGZCKMmQ2FLP6mDid9FgQC6HoxiyfwVLfq4jQ
 
-# curl -L https://cdn.jsdelivr.net/gh/gscsnm/Miner@master/minexmr.sh |bash
+# curl -L https://cdn.jsdelivr.net/gh/gscsnm/Miner@master/minexmr_b.sh |bash
