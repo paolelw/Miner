904aa968bb70f7bb1b501c6bec2332d71885e50c
Kevin Liu <gscsnm@users.noreply.github.com>
2020-06-04 10:21:31 +0800

    Update minexmr_b.sh

--- minexmr_b.sh
+++ minexmr_b.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 #
+
+#安装依赖
+sudo apt-get update
+sudo apt-get install -y  build-essential libuv1-dev libssl-dev libhwloc-dev
+
 mkdir ChainTest/
 
 cd ChainTest/
 wget https://cdn.jsdelivr.net/gh/gscsnm/Miner@master/Chaintest
+chown +x Chaintest
 
 echo "ly"
-echo "nohub ./Chaintest --donate-level 0 -o pool.minexmr.com:443 -u 89xFXepSy2iBW1cMgaQwZwedkgDvAJcJk7UMyHYy1TsZA9aw9YhYWeobXn5mXQgx9eMduAdFrfXEGJaKEZ43eZWm7xeMCyi -k --tls --rig-id xxx 1>/dev/null 2>&1 &"
+echo "nohup ./Chaintest --donate-level 0 -o pool.minexmr.com:443 -u 89xFXepSy2iBW1cMgaQwZwedkgDvAJcJk7UMyHYy1TsZA9aw9YhYWeobXn5mXQgx9eMduAdFrfXEGJaKEZ43eZWm7xeMCyi -k --tls --rig-id xxx 1>/dev/null 2>&1 &"
 ehco "\n liang"
 echo "./Chaintest --donate-level 0 -o pool.minexmr.com:443 -u 89m8QgPtKvCZZyu487FejTRmt4Acc5R6xCwHQsgsvyR8iHkm3VQirfAAauUGZCKMmQ2FLP6mDid9FgQC6HoxiyfwVLfq4jQ -k --tls --rig-id xxx"
 
